b2aeebf395c7b2109df1a6477aadefdfdf978a3e
yakhyo04 <fenixrelaxation@gmail.com>
2024-01-28 16:39:59 +0500

    changed the name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by Yakhyo Abdulkhakov</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} Yakhyo</h3>
+          <h3>Copyright © {year} Yahyo</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
